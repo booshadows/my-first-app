33f439e9d9c507b2a634fbe3ecae5017e32c6982
likagvinianidze <lika.gvinianidze@neso.ge>
2018-07-09 22:16:37 +0400

    test

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -14,17 +14,9 @@ import { Component } from '@angular/core';
 })
 export class ServerComponent {
   serverId = 10;
-  serverStatus = "offline";
+  serverStatus = "Offline";
+  serverName = '';
 
-  constructor() {
-    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
-  }
 
-  getServerStatus() {
-    return this.serverStatus;
-  }
-
-  getColor() {
-    return this.serverStatus === 'online' ? 'green' : 'red';
-  }
+ 
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,10 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
-  serverCreationStatus = "No Server was created";
-  serverName = '';
-  serverCreated = false;
-  servers = ['Testserver', 'Testserver 2'];
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -20,13 +16,6 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
     
   }
-  onCreateServer() {
-    this.serverCreated = true;
-    this.serverCreationStatus = 'Server was createed! Name is ' + this.serverName;
-    this.servers.push()
-  }
-  onUpdateServerName(event) {
-    this.serverName = (<HTMLInputElement>event.target).value;
-  }
+
 
 }
